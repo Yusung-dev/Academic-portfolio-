ec7226576a6852240386a6b7b685f55cb0535472
Yusung Choi <cyscyb@gmail.com>
2025-08-03 15:16:44 +0900

    Update models.py

--- paper-reviews/ImageStyleTransfer_CNN/구현/models.py
+++ paper-reviews/ImageStyleTransfer_CNN/구현/models.py
@@ -1,6 +1,45 @@
+#import
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from torchvision.models import vgg19
 
-print("Hello World")
+conv = {
+    'conv1_1' : 0, #style featuremap layer 
+    'conv2_1' : 5, #style featuremap layer 
+    'conv3_1' : 10, #style featuremap layer 
+    'conv4_1' : 19, #style featuremap layer 
+    'conv5_1' : 28, #style featuremap layer 
+    'conv4_2' : 21, #content featuremap layer
+}
 
+#model 만들기
+class StyleTransfer(nn.Module):
+    def __init__(self,):
+        super(StyleTransfer, self).__init__()
+        #TODO: VGG19 load
+        self.vgg19_model = vgg19(pretrained = True)
+        self.vgg19_features = self.vgg19_model.features
+
+        #TODO: conv layer 분리
+        self.style_layer = [conv['conv1_1'], conv['conv2_1'], conv['conv3_1'], conv['conv4_1'], conv['conv5_1']]
+        self.content_layer = [conv['conv4_2']]
+
+        pass
+
+    def forward(self, x, mode:str):
+        #TODO : style, content마다 conv layer slicing해서 사용하기
+        features = []
+
+        if mode == 'style':
+            for i in range(len(self.vgg19_features)):
+                x = self.vgg19_features[i](x)
+                if i in self.style_layer:
+                    features.append(x)
+
+        if mode == 'content':
+            for i in range(len(self.vgg19_features)):
+                x = self.vgg19_features[i](x)
+                if i in self.content_layer:
+                    features.append(x)
+
+        return features
